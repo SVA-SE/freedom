a951032793885d3fc669c82b0a8369d129d3e252
Thomas Rosendal <thomas.rosendal@sva.se>
2016-10-28 13:42:19 +0200

    Added example with multiple herd and animal URG
    5 herd level risk groups and 5 animal level risk groups

--- tests/multi_risk_herd_and_animal.R
+++ tests/multi_risk_herd_and_animal.R
@@ -55,3 +55,74 @@ post_pf <- post_fr(0.5, system_sens)
 ## Prior probability at next year assuming an annual risk of
 ## introduction of 0.05%
 stopifnot(identical(round(prior_fr(post_pf, 0.05), 15), 0.642596871541303))
+rm(list = ls())
+###
+library(freedom)
+## 100 herds,
+df <- sample_data(nherds = 200,
+                 mean_herd_size = 300,
+                 n_herd_urg = 5,
+                 herd_dist = c(0.4, 0.2, 0.2, 0.1, 0.1),
+                 herd_samp_frac = 0.1,
+                 herd_samp_dist = c(0.2, 0.3, 0.3, 0.1, 0.1),
+                 n_animal_urg = 5,
+                 animal_dist = c(0.5, 0.2, 0.1, 0.1, 0.1),
+                 animal_samp_frac = 0.1,
+                 animal_samp_dist = c(0.1, 0.3, 0.3, 0.1, 0.2),
+                 seed = 1)
+## write.csv2(df, file = "~/Desktop/data.csv")
+##
+## Now there are 5 herd level risk groups to use to calculate the HSe.
+## The relative risks are
+## URG1 == 1
+## URG2 == 2.3
+## URG3 == 1.5
+## URG4 == 9
+## URG5 == 3
+## Now there are 5 animal risk groups, the animal relative risks are:
+## The relative risks are
+## URG1 == 1
+## URG2 == 3
+## URG3 == 1.5
+## URG4 == 2
+## URG5 == 4
+##
+## The Design prevalence for the calculation below is 2% at the
+## herd level and 15% at the animal level. The sensitivity of the test
+## is 70%. We will calculate the Sensitivity of the surveillance
+## system.
+##
+## First calculate the Adjusted risks for each Herd unit risk group
+##
+##Since there are 2 rows for every herds the following is OK
+table(df$herd_urg)/nrow(df)
+## Now calculate the EPHI for each herdtype
+AR <- freedom::adjusted_risk(as.numeric(table(df$herd_urg)/nrow(df)), c(1, 2.3, 1.5, 9, 3))
+EPHI <- freedom::EffProbInf(0.02, AR)
+EPHI <- EPHI[df$herd_urg[match(unique(df$ppn), df$ppn)]]
+## Now calculate the EPAI for each animal category in each herd
+df$epai <- unlist(
+lapply(unique(df$ppn), function(x){
+    df <- df[df$ppn == x,]
+    prop <- df$N_animal_urg / sum(df$N_animal_urg)
+    freedom::EffProbInf(0.15, freedom::adjusted_risk(prop, c(1, 3, 1.5, 2, 4)))
+})
+)
+df$ephi <- EPHI[df$herd_urg]
+## Then use this in the Herd sensitivity Calculation
+##
+HSE <- do.call("c", lapply(unique(df$ppn), function(x){
+    freedom::hse(df$n_animal_urg[df$ppn == x],
+                 df$N_animal_urg[df$ppn == x],
+                 0.70, df$epai[df$ppn == x])
+}))
+## Then the system sensitivity
+system_sens <- sysse(EPHI, HSE)
+## Posterior probability of freedom.
+##
+## This is calculated based on the prior probabiltiy of freedom and
+## the sensitivity of the surveillance system.
+post_pf <- post_fr(0.5, system_sens)
+## Prior probability at next year assuming an annual risk of
+## introduction of 0.05%
+stopifnot(identical(round(prior_fr(post_pf, 0.05), 15), 0.636108664339913))
