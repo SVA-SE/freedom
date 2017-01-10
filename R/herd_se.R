8286ec1b11dfb02fe6f3e0ad85cc65408b225e15
Thomas Rosendal <thomas.rosendal@sva.se>
2017-01-10 11:56:59 +0100

    First draft of changes
    Have not inspected the tests yet.

--- R/herd_se.R
+++ R/herd_se.R
@@ -7,32 +7,39 @@
 ##' when greater than 10% of the population has been sampled
 ##' 
 ##' @title hse_finite
+##' @param id The herdid.
 ##' @param n_tested The number tested in each URG
 ##' @param N The number of units in each of the URG
-##' @param test_Se The sensitivity of the test (length = 1). If you
-##'     have reason to believe that the test sensitivity is different
-##'     for different URG. Then supply a vector of Sensitivities. This
-##'     could conceivably be because of using different tests for
-##'     different samples from different URG.
-##' @param dp The is a vector (length 1) of the the design prevalance
-##'     (df) in the case where there is only one unit risk group (URG)
-##'     in the herd. Or a vector (length n) of EPIn for each of the
-##'     URG in the herd.
-##' @return A vector (length 1)
+##' @param test_Se The sensitivity of the test. This may have length
+##'     == 1 if all URG and all herds have the same test_Se. It may
+##'     also have length(test_Se) == length(n_tested).
+##' @param dp The design prevalance (dp) could be length(dp) == 1 if
+##'     all URG and herds have the same dp. It could alternatively be
+##'     length(dp) == length(n_tested) if different design prevalances
+##'     are to be applied to each URG. 
+##' @return A data.frame. A dataframe is returned with 2 columns: "id" and HSe
 ##' @export
 ##' @author Thomas Rosendal
-hse_finite <- function(n_tested, N, test_Se, dp) {
+hse_finite <- function(id, n_tested, N, test_Se, dp) {
     if(length(n_tested) != length(N)) {
-        stop("The length of the n_tested vector must be equal to the N vector.\nie. you must describe both the number fo animals tested in each group as well as how many animals are in each group.")
+        stop("The length of the n_tested vector must be equal to the N vector.\nie. you must describe both the number of animals tested in each group as well as how many animals are in each group.")
     }
     if(any(n_tested > N)) {
         stop("One of the URG has more subunits tested than in the population")
     }
-    if(length(n_tested) != length(dp)) {
-        stop("The length of the n_tested vector must be equal to the dp vector.\nie. you must describe both the number fo animals tested in each group as well as the dp in each group.")
+    if(!(length(dp) == 1 | length(n_tested) == length(dp))) {
+        stop("The length of the n_tested vector must be equal to the dp vector.\nie. you must describe both the number of animals tested in each group as well as the dp in each group.")
     }
-    1 - prod((1 - (n_tested * test_Se / N)) ^ (dp * N))
+    if(!(length(test_Se) == 1 | length(test_Se) == length(n_tested))) {
+        stop("The length of test_Se must be either 1 or the length of n_tested")
+    }
+    A <- 1 - (n_tested * test_Se / N)
+    B <- dp * N
+    df <- as.data.frame(1 - tapply(A ^ B, INDEX = id, FUN = "prod"))
+    names(df) <- c("HSe")
+    return(df)
 }
+
 ##' Herd Sensitivity calculated with the assumption of an infinite population
 ##'
 ##' Calculate the Herd sensitivity when multiple samples from
@@ -42,25 +49,32 @@ hse_finite <- function(n_tested, N, test_Se, dp) {
 ##' used when less than 10% of the population has been sampled.
 ##' 
 ##' @title hse_infinite
+##' @param id The herdid
 ##' @param n_tested The number tested in each URG
-##' @param test_Se The sensitivity of the test (length = 1). If you
-##'     have reason to believe that the test sensitivity is different
-##'     for different URG. Then supply a vector of Sensitivities. This
-##'     could conceivably be because of using different tests for
-##'     different samples from different URG.
-##' @param dp The is a vector (length 1) of the the design prevalance
-##'     (df) in the case where there is only one unit risk group (URG)
-##'     in the herd. Or a vector (length n) of EPIn for each of the
-##'     URG in the herd.
-##' @return A vector (length 1)
+##' @param test_Se The sensitivity of the test. This may have length
+##'     == 1 if all URG and all herds have the same test_Se. It may
+##'     also have length(test_Se) == length(n_tested).
+##' @param dp The design prevalance (dp) could be length(dp) == 1 if
+##'     all URG and herds have the same dp. It could alternatively be
+##'     length(dp) == length(n_tested) if diff
+##' @return A data.frame. A dataframe is returned with 2 columns: "id" and HSe
 ##' @export
 ##' @author Thomas Rosendal
-hse_infinite <- function(n_tested, test_Se, dp){
-    if(length(n_tested) != length(dp)) {
+hse_infinite <- function(id, n_tested, test_Se, dp){
+    if(!(length(n_tested) == length(dp) | length(dp) == 1)) {
         stop("The length of the n_tested vector must be equal to the dp vector.\nie. you must describe both the number fo animals tested in each group as well as the dp in each group.")
     }
-    1 - prod((1 - (dp * test_Se)) ^ n_tested)
+    if(!(length(n_tested) == length(test_Se) | length(test_Se) == 1)) {
+        stop("Length of test_Se must be 1 or the length of n_tested")
+    }
+    A <- 1 - (dp * test_Se)
+    df <- as.data.frame(1 - tapply(A ^ n_tested, INDEX = id, FUN = "prod"))
+    names(df) <- c("HSe")
+    df$id <- rownames(df)
+    df <- df[,c("id", "HSe")]
+    return(df)
 }
+
 ##' Herd Sensitivity 
 ##'
 ##' Calculate the Herd sensitivity when multiple samples from
@@ -95,6 +109,7 @@ hse <- function(n_tested, N, test_Se, dp){
         return(result)
     }
 }
+
 ##' Calculate the surveillance system sensitivity
 ##'
 ##' Takes a vector of the sensitivity of herds tested in the
