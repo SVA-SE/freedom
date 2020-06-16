0cd2efa91df976f197bccd590a8dd3e437aadd64
Thomas Rosendal <thomas.rosendal@sva.se>
2020-06-16 13:22:10 +0200

    modified HSe calcualtion to allow n/N >1 with force argument

--- R/herd_se.R
+++ R/herd_se.R
@@ -5,7 +5,7 @@
 ##' population size to adjust the estimates consistent with a finite
 ##' population. This method for calculation of HSe is typically used
 ##' when greater than 10% of the population has been sampled
-##' 
+##'
 ##' @title hse_finite
 ##' @param id The herdid.
 ##' @param n_tested The number tested in each URG
@@ -16,19 +16,24 @@
 ##' @param dp The design prevalance (dp) could be length(dp) == 1 if
 ##'     all URG and herds have the same dp. It could alternatively be
 ##'     length(dp) == length(n_tested) if different design prevalances
-##'     are to be applied to each URG. 
+##'     are to be applied to each URG.
 ##' @return A data.frame. A dataframe is returned with 2 columns: "id" and HSe
 ##' @export
 ##' @author Thomas Rosendal
 hse_finite <- function(id, n_tested, N, test_Se, dp) {
     if(length(n_tested) != length(N)) {
-        stop("The length of the n_tested vector must be equal to the N vector.\nie. you must describe both the number of animals tested in each group as well as how many animals are in each group.")
+        stop(paste("The length of the n_tested vector must be equal to the N vector.",
+                   "ie. you must describe both the number of animals tested in each",
+                   "group as well as how many animals are in each group.",
+                   sep = "\n"))
     }
     if(any(n_tested > N)) {
         stop("One of the URG has more subunits tested than in the population")
     }
     if(!(length(dp) == 1 | length(n_tested) == length(dp))) {
-        stop("The length of the n_tested vector must be equal to the dp vector.\nie. you must describe both the number of animals tested in each group as well as the dp in each group.")
+        stop(paste("The length of the n_tested vector must be equal to the dp vector.",
+                   "ie. you must describe both the number of animals tested in each",
+                   "group as well as the dp in each group.", sep = "\n"))
     }
     if(!(length(test_Se) == 1 | length(test_Se) == length(n_tested))) {
         stop("The length of test_Se must be either 1 or the length of n_tested")
@@ -49,7 +54,7 @@ hse_finite <- function(id, n_tested, N, test_Se, dp) {
 ##' population size to adjust the estimate. This is consistent with
 ##' the assumption of an infinite population size and is generally
 ##' used when less than 10% of the population has been sampled.
-##' 
+##'
 ##' @title hse_infinite
 ##' @param id The herdid
 ##' @param n_tested The number tested in each URG
@@ -64,7 +69,9 @@ hse_finite <- function(id, n_tested, N, test_Se, dp) {
 ##' @author Thomas Rosendal
 hse_infinite <- function(id, n_tested, test_Se, dp){
     if(!(length(n_tested) == length(dp) | length(dp) == 1)) {
-        stop("The length of the n_tested vector must be equal to the dp vector.\nie. you must describe both the number fo animals tested in each group as well as the dp in each group.")
+        stop(paste("The length of the n_tested vector must be equal to the dp vector.",
+                   "ie. you must describe both the number fo animals tested in each",
+                   "group as well as the dp in each group."), sep = "\n")
     }
     if(!(length(n_tested) == length(test_Se) | length(test_Se) == 1)) {
         stop("Length of test_Se must be 1 or the length of n_tested")
@@ -77,13 +84,13 @@ hse_infinite <- function(id, n_tested, test_Se, dp){
     return(df)
 }
 
-##' Herd Sensitivity 
+##' Herd Sensitivity
 ##'
 ##' Calculate the Herd sensitivity when multiple samples from
 ##' individual units within the herd. The function uses the assumption
 ##' of finite population when greater than 10% of units are tested and
 ##' otherwise the assumption of infinite population.
-##' 
+##'
 ##' @title hse
 ##' @param n_tested The number tested in each URG
 ##' @param N The number of units in each of the URG
@@ -96,20 +103,22 @@ hse_infinite <- function(id, n_tested, test_Se, dp){
 ##'     (df) in the case where there is only one unit risk group (URG)
 ##'     in the herd. Or a vector (length n) of EPIn for each of the
 ##'     URG in the herd.
+##' @param force If force = FALSE (default) then the function errors
+##'     if n>N. If force = TRUE then this is allowed and uses the
+##'     hse_infinite to calculate HSe.
 ##' @return A vector (length 1)
 ##' @export
 ##' @author Thomas Rosendal
-hse <- function(n_tested, N, test_Se, dp){
-    if(sum(n_tested)/sum(N)>1) {
+hse <- function(n_tested, N, test_Se, dp, force = FALSE){
+    ratio <- sum(n_tested)/sum(N)
+    if(ratio > 1 & !force) {
         stop("Greater than 100% of animals cannot be tested")
     }
-    if(sum(n_tested)/sum(N) > 0.1) {
+    if(ratio > 0.1 & ratio < 1) {
         result <- hse_finite(n_tested, N, test_Se, dp)
         return(result)
-    } else {
-        result <- hse_infinite(n_tested, test_Se, dp)
-        return(result)
     }
+    hse_infinite(n_tested, test_Se, dp)
 }
 
 ##' Calculate the surveillance system sensitivity
@@ -126,7 +135,9 @@ hse <- function(n_tested, N, test_Se, dp){
 ##' @author Thomas Rosendal
 sysse <- function(dp, hse) {
     if(length(hse) != length(dp)) {
-        stop("The herd Se vector (hse) must be the same length as the Effective probability of infection of the herd (dp)")
+        stop(paste("The herd Se vector (hse) must be the same length",
+                   "as the Effective probability of infection of the herd (dp)",
+                   sep = "\n"))
     }
     if(any(hse > 1)) {
         stop("At least one of the hse values is greater than 1")
@@ -143,7 +154,7 @@ sysse <- function(dp, hse) {
 ##' surveillance system and a vector of the effective probability of
 ##' infection in the herds (EPIH) to calculate the total surveillance
 ##' system sensitivity for the entire program. This is adjusted for
-##' the the total number of herds in the population. 
+##' the the total number of herds in the population.
 ##' @title sysse
 ##' @param dp The vector of EPIH for all herds tested in the
 ##'     surveillance system.
@@ -155,7 +166,9 @@ sysse <- function(dp, hse) {
 ##' @author Thomas Rosendal
 sysse_finite <- function(dp, hse, N) {
     if(length(hse) != length(dp)) {
-        stop("The herd Se vector (hse) must be the same length as the Effective probability of infection of the herd (dp)")
+        stop(paste("The herd Se vector (hse) must be the same length",
+             "as the Effective probability of infection of the herd (dp)",
+             sep = "\n"))
     }
     if(any(hse > 1)) {
         stop("At least one of the hse values is greater than 1")
