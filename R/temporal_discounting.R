cc5e9fabcc00277d219bbbca6c5524bfef7df33c
Thomas Rosendal <thomas.rosendal@sva.se>
2020-08-24 21:42:50 +0200

    code style

--- R/temporal_discounting.R
+++ R/temporal_discounting.R
@@ -8,22 +8,26 @@
 ##' subsequent time intervals in the surveillance system, the prior
 ##' year's posterior probability of freedom is used (plus a risk of
 ##' introduction) as the prior probability in this calculation.
-##' 
-##' @title post_fr 
-##' @param prior_fr The prior probability of freedom  
+##'
+##' @title post_fr
+##' @param prior_fr The prior probability of freedom
 ##' @param Se The sensitivity of the surveillance system
 ##' @return A vector (length 1)
 ##' @export
 ##' @author Thomas Rosendal
 post_fr <- function(prior_fr, Se){
-    if(prior_fr > 1) {
+
+    if (prior_fr > 1) {
         stop("The prior probability of freedom cannot be greater than 1")
     }
-    if(Se > 1) {
+
+    if (Se > 1) {
         stop("System sensitivity cannot be greater than 1")
     }
+
     prior_fr / (1 - ((1 - prior_fr) * Se))
 }
+
 ##' Calculate the prior probability of freedom (year = k)
 ##'
 ##' In order to calculate the posterior probability of freedom (year =
@@ -31,7 +35,7 @@ post_fr <- function(prior_fr, Se){
 ##' calculated from the posterior probability of freedom (year = k-1)
 ##' from the previous year and the annual probability that the disease
 ##' is introduced into the population.
-##' 
+##'
 ##' @title prior_fr
 ##' @param post_fr The posterior probability of freedom (year = k-1)
 ##' @param intro The annual probability of introduction
@@ -39,11 +43,13 @@ post_fr <- function(prior_fr, Se){
 ##' @export
 ##' @author Thomas Rosendal
 prior_fr <- function(post_fr, intro){
-    if(post_fr > 1) {
+    if (post_fr > 1) {
         stop("The posterior probability of freedom cannot be greater than 1")
     }
-    if(intro > 1) {
+
+    if (intro > 1) {
         stop("The annual probability of introduction cannot be greater than 1")
     }
+
     post_fr - (post_fr * intro)
 }
