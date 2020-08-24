8be472170662614c8293547684b36572f3ff645b
Thomas Rosendal <thomas.rosendal@sva.se>
2020-08-24 21:26:54 +0200

    Code style....
    No way was that my code. I must have copied that from someone
    else. Unfortunately, I have not recorded the origin.

--- R/distribution.R
+++ R/distribution.R
@@ -1,6 +1,8 @@
 ##' Sample a pert distribution
 ##'
 ##' Returns samples from a pert distribution
+##'
+##'
 ##' @title rpert
 ##' @param n number of samples
 ##' @param x.min The minimum value in the sample
@@ -10,25 +12,32 @@
 ##' @import stats
 ##' @return a numeric vector of length n
 ##' @export
-##' @author Thomas Rosendal
-rpert <- function( n, x.min, x.max, x.mode, lambda = 4 ){
+rpert <- function(n,
+                  x.min,
+                  x.max,
+                  x.mode,
+                  lambda = 4) {
 
-    if( x.min > x.max || x.mode > x.max || x.mode < x.min ) stop( "invalid parameters" );
+    if (x.min > x.max || x.mode > x.max || x.mode < x.min) {
+        stop("invalid parameters")
+    }
 
-    x.range <- x.max - x.min;
-    if( x.range == 0 ) return( rep( x.min, n ));
+    x.range <- x.max - x.min
+    if (x.range == 0) {
+        return(rep(x.min, n))
+    }
 
-    mu <- ( x.min + x.max + lambda * x.mode ) / ( lambda + 2 );
+    mu <- (x.min + x.max + lambda * x.mode) / (lambda + 2)
 
-    # special case if mu == mode
-    if( mu == x.mode ){
-        v <- ( lambda / 2 ) + 1
+    ## special case if mu == mode
+    if (mu == x.mode) {
+        v <- (lambda / 2) + 1
     }
     else {
-        v <- (( mu - x.min ) * ( 2 * x.mode - x.min - x.max )) /
-            (( x.mode - mu ) * ( x.max - x.min ));
+        v <- ((mu - x.min) * (2 * x.mode - x.min - x.max)) /
+            ((x.mode - mu) * (x.max - x.min))
     }
 
-    w <- ( v * ( x.max - mu )) / ( mu - x.min );
-    return ( rbeta( n, v, w ) * x.range + x.min );
+    w <- (v * (x.max - mu)) / (mu - x.min)
+    return (rbeta(n, v, w) * x.range + x.min)
 }
