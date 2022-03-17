83c2cf77fc6073289574c86caf0fa9dcd15749f2
Thomas Rosendal <thomas.rosendal@sva.se>
2022-03-17 14:44:01 +0100

    Add tests of new method

--- tests/hse_finite.R
+++ tests/hse_finite.R
@@ -4,7 +4,7 @@ tol <- 1e-7
 library(freedom)
 
 ## expect hse to be less than 1 even though Se is 1 since you don't test all
-ob <- hse_finite(1, 10, 20, 1, 0.1)$Hse
+ob <- hse_finite(1, 10, 20, 1, 0.1)$HSe
 stopifnot(ob < 1)
 
 ## expect hse to be 1 when Se is 1 since you do test all
@@ -36,3 +36,23 @@ stopifnot(nrow(ob) == 2L)
 ob <- tools::assertError(hse_finite(1, c(10, 10), c(25, 25), 0.8, 0.1))[[1]]$message
 ex <- "Argument id \\(grouping variable\\) should be the same length as n_tested"
 stopifnot(length(grep(ex, ob)) == 1L)
+
+
+## check what happens when you use rounding
+ob1 <- hse_finite(1, 1, 20, 1, 0.01, "round")$HSe
+stopifnot(abs(ob1 - 0.05) < tol)
+
+ob2 <- hse_finite(1, 1, 20, 1, 0.01, "floor")$HSe
+ob3 <- hse_finite(1, 1, 20, 1, 0.01, "ceiling")$HSe
+ob4 <- hse_finite(1, 1, 20, 1, 0.05)$HSe
+stopifnot(identical(ob1, ob2, ob3, ob4))
+
+## expect the floor method to be more conservative than the ceiling method
+ob1 <- hse_finite(1, 1, 20, 1, 0.06, "ceiling")$HSe
+ob2 <- hse_finite(1, 1, 20, 1, 0.06, "floor")$HSe
+stopifnot(ob1 > ob2)
+
+## expect the 'none' method to be more conservative than round for small fractions
+ob1 <- hse_finite(1, 1, 20, 1, 0.01, "round")$HSe
+ob2 <- hse_finite(1, 1, 20, 1, 0.01, "none")$HSe
+stopifnot(ob1 > ob2)
